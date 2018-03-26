4465c1bc2e177cbbd8202706a1b4ee30c986cd85
DM-Star <37034961+DM-Star@users.noreply.github.com>
2018-03-26 18:45:04 +0800

    2018.3.26
    更新了WordList类的定义

--- wcPro/wcPro/WordList.h
+++ wcPro/wcPro/WordList.h
@@ -18,14 +18,26 @@ struct Word {
 	}
 };
 
+struct WordIndex {
+	Word *pWord;
+	WordIndex *next;
+};
+
 //这个类用于操作所有单词的词频
 //需要实现：可以快速地按顺序取出最高词频的100个词
 //以及：可以根据给出的单词名称（码），立即找到该单词的存储单元。
 class WordList
 {
+private:
+	Word *hWord;				//单词链表的头指针
+	WordIndex *index[128];		//哈希索引
+
+	int hash(char *word);		//哈希函数
+
 public:
 	WordList();
 	~WordList();
-	void addWord(char word[]);
+	void addWord(char word[]);	//给定一个单词，使这个单词的词频+1
+								//如果没有这个单词，则在单词链表添加这个单词，并加入到索引中
 	void outPut();
 };
